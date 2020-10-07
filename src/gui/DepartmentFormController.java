64cabe0f7f083323d7309b8edcd6c11711afc833
Lael Andrade <laelandrade@gmail.com>
2020-10-07 16:31:38 -0300

    Update Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -57,9 +57,7 @@ public class DepartmentFormController implements Initializable {
 	public void subscribeDataChangeListener(DataChangeListener listener) {
 		dataChangeListeners.add(listener);
 	}
-	
-	
-	
+		
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
 		if (entity == null) {
